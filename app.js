8a617a8ea8cf5508b7376cf0a56d6190c1add25a
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2023-09-12 19:31:34 +0200

    Add functions for calculator and test them

---
+++ app.js
@@ -0,0 +1,20 @@
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a, b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+console.log(add(4,5));
+console.log(subtract(4,5));
+console.log(multiply(4,5));
+console.log(divide(4,5));
