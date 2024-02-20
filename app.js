ab05fa0a7a66202add33ce85a1428d0307fea350
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-02-20 18:45:36 +0100

    Add calculation operations to clicking function
    It is not fully fixed, since it's buggy now.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let displayValue = '';
-let number1 = "";
-let number2 = "";
+let displayValue = "";
+let num1 = "";
+let num2 = "";
 let operator = "";
 
 const buttons = document.querySelectorAll("button");
@@ -23,9 +23,30 @@ function populateDisplay(){
 function clickingButton(){
     buttons.forEach(button => button.addEventListener("click", () =>{
         // Checks if the length of display exceeds 9
-        if (displayValue.length < 9){
+        if (button.classList.contains("number")){
+            if (displayValue.length < 9){
+                displayValue += button.innerText;
+                populateDisplay();
+                if (operator === ""){
+                    num1 = displayValue;
+                    console.log(num1);
+                }else{
+                    num2 = displayValue;
+                    console.log(num2);
+                }
+            } 
+        }else if (button.classList.contains("operator")){
             displayValue += button.innerText;
-            populateDisplay();
+            operator = button.innerText;
+        }else if (button.innerText === "="){
+            if (num1 !== "" && num2 !== "" && operator !== ""){
+                let result = operate(parseFloat(num1), parseFloat(num2), operator);
+                displayValue = result.toString();
+                populateDisplay();
+                num1 = result.toString();
+                num2 = "";
+                operator = "";
+            }
         }
     }));
 }
