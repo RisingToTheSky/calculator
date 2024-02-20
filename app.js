29b9711cd1abb96290664a7a9cec7296f71bd44e
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-02-20 14:46:04 +0100

    Add populatedisplay function

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let number1;
-let number2;
-let operator;
+let displayValue = '0';
+let number1 = "";
+let number2 = "";
+let operator = "";
 
-const button = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
+const button = document.querySelector("button");
+const number = document.getElementsByClassName(".number");
 
 
 function operate(a, b, operator){
@@ -12,7 +15,14 @@ function operate(a, b, operator){
     if (operator === "/") return a/b;
 }
 
-button.addEventListener("click", () => {
-    
-})
+// Populate display
+function populateDisplay(){
+    const display = document.querySelector('.display');
+    // Set text content to value of button
+    display.innerTextContent = displayValue;
+    if (displayValue.length > 9){
+        display.innerTextContent = displayValue.substring(0, 9);
+    }
+}
 
+populateDisplay();
