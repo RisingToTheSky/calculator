96fe852e734f41f5e54086aa43a2e1f5e1b019e6
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-02-22 11:30:20 +0100

    Add backspace button

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let displayValue = "";
 let num1 = "";
 let num2 = "";
 let operator = "";
+let result;
 
 const buttons = document.querySelectorAll("button");
 
@@ -14,7 +15,7 @@ function operate(a, b, operator){
         if (b !== 0){
             return a/b;
         }else{
-            return "MATH ERROR";
+            return "ERROR";
         }
     }
 }
@@ -46,34 +47,29 @@ function clickingButton(){
                 displayValue = "";
                 populateDisplay();
             }
-        }else if (button.innerText === "="){
-            if (num1 !== "" && num2 !== "" && operator !== ""){
-                let result = operate(parseFloat(num1), parseFloat(num2), operator);
-                // Round results when it exceeds 7 decimals
-                if (result.toString().length > 9){
-                    displayValue = result.toFixed(7);
-                }else{
-                    displayValue = result.toString();
-                }
+        }else if (num1 !== "" && num2 !== "" && operator !== ""){
+                result = operate(parseFloat(num1), parseFloat(num2), operator);
+                displayValue = result.toString();
                 populateDisplay();
                 num1 = result.toString();
                 num2 = "";
                 operator = "";
             }
-        }else if (button.innerText !== "="){
-            // Evaluate only one pairs
-
-            // Always store result after 2 numbers
-
-            // When equals sign is clicked, change result
-        }
         // Add clear button 
         if (button.classList.contains("clear")){
             displayValue = "";
             populateDisplay();
+            num1 = "";
+            num2 = "";
+            operator = "";
+        }
+
+        // Add backspace button
+        if (button.classList.contains("clearEntry")){
+            displayValue = displayValue.substring(0, displayValue.length - 1);
+            populateDisplay();
         }
     }));
 }
-
 
 clickingButton();
