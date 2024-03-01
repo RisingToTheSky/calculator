02fa35fbf1d2209b2bf3942a5688fc7e334907d5
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-03-01 18:44:07 +0100

    Add rounding decimals functionality

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ let num2 = "";
 let operator = "";
 let result;
 let operatorClicked = false;
-
 const buttons = document.querySelectorAll("button");
 
 
@@ -20,64 +19,77 @@ function operate(a, b, operator){
         }
     }
 }
+
 function evaluate(){
-    if (num1 !== "" && num2 !== "" && operator !== "") {
+    if (num1 !== "" && num2 !== "" && operator !== ""){
         result = operate(parseFloat(num1), parseFloat(num2), operator);
-        displayValue = result.toString();
-        populateDisplay();
         num1 = result.toString();
         num2 = "";
         operator = "";
+        displayValue = num1;
+        populateDisplay();
     }
 }
 function populateDisplay(){
     const display = document.querySelector('.display');
+    if (displayValue.length > 9){
+        displayValue = parseFloat(displayValue);
+        displayValue = displayValue.toExponential(2);
+    }
     display.innerText = displayValue;
-}
-// When button with class of number is clicked, it should display
-function clickingButton(){
-    buttons.forEach(button => button.addEventListener("click", () =>{
-        // Checks if the length of display exceeds 9
-        if (button.classList.contains("number")){
-            if (displayValue.length < 9){
-                displayValue += button.innerText;
-                populateDisplay();
-                if (operator === ""){
-                    num1 = displayValue;
-                    console.log(num1);
-                }else{
-                    num2 = displayValue;
-                    console.log(num2);
-                }
-            } 
-        }else if (button.classList.contains("operator")){
-            if (num1 !== "" && operator !== ""){
-                evaluate();
-            }
-            operator = button.innerText;
-            operatorClicked = true;
-            if (num1 !== ""){
-                displayValue = "";
-                populateDisplay();
-            }
-        }else if (num1 !== "" && num2 !== "" && operator !== ""){
-            evaluate();
-        }else if (button.classList.contains("equals")){
-            evaluate();
-            operatorClicked = false;
-        }
-        if (button.classList.contains("clear")){
-            displayValue = "";
-            populateDisplay();
-            num1 = "";
-            num2 = "";
-            operator = "";
-        }
-        if (button.classList.contains("clearEntry")){
-            displayValue = displayValue.substring(0, displayValue.length - 1);
-            populateDisplay();
-        }
-    }));
+    if (display.innerText == ""){
+        display.innerText = "0";
+    }
 }
 
-clickingButton();
+// When button with class of number is clicked, it should display
+buttons.forEach(button => button.addEventListener("click", () =>{
+    // Checks if the length of number exceeds 9
+    if (button.classList.contains("number")){
+        if (displayValue.length < 9){
+            displayValue += button.innerText;
+            populateDisplay();
+            if (operator === ""){
+                num1 = displayValue;
+                console.log(num1);
+            }else{
+                num2 = displayValue;
+                console.log(num2);
+            }
+        } 
+    }else if (button.classList.contains("operator")){
+        if (num1 !== "" && operator !== ""){
+            evaluate();
+        }
+        operator = button.innerText;
+        operatorClicked = true;
+        if (num1 !== ""){
+            displayValue = "";
+            populateDisplay();
+        }
+    }else if ((num1 !== "" && num2 !== "" && operator !== "")){
+        evaluate();
+    }else if (button.classList.contains("equals")){
+        evaluate();
+        operatorClicked = false;
+    }
+    if (button.classList.contains("clear")){
+        displayValue = "";
+        populateDisplay();
+        num1 = "";
+        num2 = "";
+        operator = "";
+    }
+    if (button.classList.contains("clearEntry")){
+        displayValue = displayValue.substring(0, displayValue.length - 1);
+        populateDisplay();
+    }
+    if (button.classList.contains("decimal")){
+        if (button.innerText === "."){
+            if (!displayValue.includes(".") && displayValue !== ""){
+                displayValue += ".";
+                populateDisplay();
+            }
+        }
+    }
+}));
